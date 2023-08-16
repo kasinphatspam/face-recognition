a1ce39e36e0a5dfb560c82c77d3bf6adce47002b
kasinphatspam <kasinphat@icloud.com>
2023-08-17 02:40:01 +0700

    refractor: protect system generate same key

--- ml-server/file.py
+++ ml-server/file.py
@@ -1,14 +1,28 @@
 import random
 import string
+import os
 
 def generate_random_filename(length):
     characters = string.digits + string.ascii_uppercase
     random_filename = ''.join(random.choice(characters) for _ in range(length))
     return random_filename
 
-def createFile(filename):
-    file_image = f"dataset/image/{filename}.npy"  # Create a name for the name .npy file
-    file_encodeId = f"dataset/id/{filename}.npy"  # Create a name for the userID .npy file
-    open(file_image, 'wb')
-    open(file_encodeId, 'wb')
+def check_unique_filename(filename):
+    directory_list = os.listdir("dataset/image/")
+    count = 0
+    for i in directory_list:
+        if i == filename + ".npy":
+            count+=1
+    if count != 0:
+        return False
     return True
+
+def createFile(filename):
+    if check_unique_filename(filename):
+        file_image = f"dataset/image/{filename}.npy"
+        file_encodeId = f"dataset/id/{filename}.npy"
+        open(file_image, 'wb')
+        open(file_encodeId, 'wb')
+        return True
+    return createFile(generate_random_filename(8))
+    
