88382fa5a48c71f339f06c118363e3460d3dac46
Kobayashi-UwU <tai242415597@gmail.com>
2023-10-16 10:53:00 +0700

    refactor file.py

--- ml-server/file.py
+++ ml-server/file.py
@@ -3,28 +3,22 @@ import string
 import os
 import numpy as np
 
+
 def generate_random_filename(length):
     characters = string.digits + string.ascii_uppercase
-    random_filename = ''.join(random.choice(characters) for _ in range(length))
+    random_filename = "".join(random.choice(characters) for _ in range(length))
     return random_filename
 
-def check_unique_filename(filename):
-    directory_list = os.listdir("dataset/")
-    count = 0
-    for i in directory_list:
-        if i == filename + ".npy":
-            count += 1
-    if count != 0:
-        return False
-    return True
 
-def create_file(filename):
-    if check_unique_filename(filename):
+def check_unique_filename(filename, directory="dataset/"):
+    directory_list = os.listdir(directory)
+    return filename + ".npy" not in directory_list
 
+
+def create_file(filename, directory="dataset/"):
+    if check_unique_filename(filename, directory):
         face_data = {"encodings": [], "ids": []}
-        np.save(f"./dataset/{filename}.npy", face_data)
-        
+        np.save(os.path.join(directory, f"{filename}.npy"), face_data)
         return True
     else:
-        # If the filename is not unique, recursively create a new filename
-        return create_file(generate_random_filename(8))
+        return create_file(generate_random_filename(8), directory)
