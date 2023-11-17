d7f96a8225bae099a8b1513872856a34b69977c8
Secret man <kasinphat@icloud.com>
2023-11-18 00:57:33 +0700

    Merge branch 'dev' of https://github.com/kasinphatspam/face-recognition into dev

--- server/apps/backend/src/service/notification.fcm.service.ts
+++ server/apps/backend/src/service/notification.fcm.service.ts
@@ -1,4 +1,7 @@
+import { User } from '@/entity';
 import { Injectable } from '@nestjs/common';
+import { app } from 'firebase-admin';
+import { applicationDefault, initializeApp } from 'firebase-admin/app';
 
 @Injectable()
 export class NotificationFCMService {}
--- server/apps/frontend/src/components/Card/FeatureCard.jsx
+++ server/apps/frontend/src/components/Card/FeatureCard.jsx
@@ -3,9 +3,9 @@ import { Card, CardHeader, CardBody } from "@nextui-org/react";
 export default function FeaturesCard({ head, desc }) {
     return (
         <>
-            <Card isBlurred shadow="none" className="min-w-[200px] overflow-hidden max-w-[500px] border-small border-white/50 dark:border-white/20 bg-transparent/20 text-white">
+            <Card isBlurred shadow="none" className="shadow-xl min-w-[200px] overflow-hidden max-w-[500px] border-small border-[#ffffff] dark:border-[#ffffff] bg-white text-black dark:text-white">
                 <CardHeader>
-                    <div className="font-semibold ml-[8px] text-lg">{head}</div>
+                    <div className=" text-cyan-500 font-semibold ml-[8px] text-lg">{head}</div>
                 </CardHeader>
                 <CardBody>
                     <div className="min-w-[150px] max-sm:-mt-4 text-sm -mt-4">
